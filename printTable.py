243341f35793b8d7a6c325ed401ae74b146b982a
Shi Jianjun <1228629207@qq.com>
2016-12-03 21:56:45 +0800

    add a printTable.py file

---
+++ printTable.py
@@ -0,0 +1,29 @@
+'''
+Write a function named printTable() that takes a 
+list of lists of strings and displays it in a well-
+organized table with each column right-justified. 
+Assume that all the inner lists will contain the 
+same number of strings.
+'''
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+			['Alice', 'Bob', 'Carol', 'David'],
+			['dogs', 'cats', 'moose', 'goose']]
+
+def printTable(tableList):
+	lengthList = []
+	length = 0
+	for i in range(len(tableList)):
+		for j in range(len(tableList[i])):
+			if length <= len(tableList[i][j]):
+				length = len(tableList[i][j])
+		lengthList.append(length)
+
+		length = 0
+
+	for j in range(len(tableList[0])):
+		width = 0
+		for i in range(len(tableList)):
+			width = lengthList[i] + 1
+			print(tableList[i][j].rjust(width), end = '')
+		print()
+printTable(tableData)
