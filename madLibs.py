05aedf583d9ded7fc9c17683c19ecae06b47d47e
Shi Jianjun <1228629207@qq.com>
2016-12-05 23:49:31 +0800

    simply the code

--- madLibs.py
+++ madLibs.py
@@ -1,29 +1,22 @@
 import re
 
+fileRegex = re.compile('ADJECTIVE|NOUN|VERB')
 fhand = open('madLibs.txt')
 fileContent = fhand.read()
-print(fileContent)
 fhand.close()
-fileRegex = re.compile('ADJECTIVE|NOUN|VERB')
-
-print('Enter an adjective:')
-adj = input()
-fileContent = fileRegex.sub(adj, fileContent, count = 1)
-#print(fileContent)
-
-print('Enter a noun:')
-nounFst = input()
-fileContent = fileRegex.sub(nounFst,fileContent, count=1 )
-
-print('Enter a verb:')
-verb = input()
-fileContent = fileRegex.sub(verb,fileContent, count = 1)
-
-print('Enter a noun:')
-nounScd = input()
-fileContent = fileRegex.sub(nounScd, fileContent, count = 1)
 print(fileContent)
 
-file = open('madLibs.txt', 'w')
-file.write(fileContent)
-file.close()print(fileContent)
+adj = input('Enter an abjective:\n')
+firstNoun = input('Enter a noun:\n')
+verb = input('Enter a verb:\n')
+secondNoun = input('Enter a noun:\n')
+
+words = [adj, firstNoun, verb, secondNoun]
+
+for i in range(len(words)):
+	fileContent = fileRegex.sub(str(words[i]), fileContent, count = 1)
+
+print(fileContent)
+
+file = open('madLib.txt',  'w')
+file.close()
